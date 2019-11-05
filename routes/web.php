cd86ea48fd76a3cc09c9e4aac5512a40db19ca1d
CaptainAntbill <mejiaanthony016@gmail.com>
2019-11-04 20:29:03 -0600

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::resource('spectrum', 'SpectrumController');
 Route::resource('provider', 'ProviderController');
 Route::resource('phone', 'PhoneController');
 Route::resource('purchase', 'PurchaseController');
+Route::resource('accessory', 'AccessoryController');
+Route::resource('purchase_accessory', 'PurchaseAccessoryController');
