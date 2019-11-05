5045e0721205413b59ed83b40ad65b76f9d7ae28
CaptainAntbill <mejiaanthony016@gmail.com>
2019-11-04 20:30:52 -0600

    Created migration accesories

---
+++ database/migrations/2019_11_04_204414_create_accessories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccessoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accessories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->bigInteger('brand_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('brand_id')->references('id')->on('brands');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accessories');
+    }
+}
