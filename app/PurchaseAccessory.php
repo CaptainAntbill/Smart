81506f28ef1e03fd81d72d8a7edc9ee1fe9c5a90
CaptainAntbill <mejiaanthony016@gmail.com>
2019-11-10 18:44:58 -0600

    Modify model PurchaseAccessory

--- app/PurchaseAccessory.php
+++ app/PurchaseAccessory.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class PurchaseAccessory extends Model
 {
-    protected $table = 'purchase_accessory';
+    protected $table = 'purchase_accessories';
     protected $fillable = ['order', 'price', 'date_order', 'accessory_id', 'provider_id'];
 
     public function provider(){
         return $this->belongsTo('App\Provider');
     }
 
-    public function phone(){
+    public function accessory(){
         return $this->belongsTo('App\Accessory');
     }
 }
