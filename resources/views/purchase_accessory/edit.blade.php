e01263dcd74f0b2772191fdee3ab4681d7dec109
CaptainAntbill <mejiaanthony016@gmail.com>
2019-11-19 23:16:07 -0600

    Start design for purchase accessory

--- resources/views/purchase_accessory/edit.blade.php
+++ resources/views/purchase_accessory/edit.blade.php
@@ -1,36 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
-
-<body>
-    <form action="{{ route('purchase_accessory.update', $purchase->id) }}" method="post">
-        @csrf
-        @method('PATCH')
-        {{ csrf_field() }}
-        <input type="text" name="order" required placeholder="Orden" value="{{ $purchase->order }}"><br>
-        <input type="text" name="price" required placeholder="Precio" value="{{ $purchase->price }}"><br>
-        <input type="text" name="date_order" required placeholder="Fecha de Orden" value="{{ $purchase->date_order }}"><br>
-        <select name="provider_id" class="select">
-            @foreach($provider as $provider)
-            <option value="{{$provider['id']}}"> {{$provider['name']}} </option>
-            @endforeach
-        </select>
-        <select name="accessory_id" class="select">
-            @foreach($accessory as $accessory)
-            <option value="{{$accessory['id']}}"> {{$accessory['name']}} </option>
-            @endforeach
-        </select>
-        <input type="submit" value="Enviar">
-    </form>
-
+<body><br>
+    <div class="container">
+        <div class="hero is-link">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-1">Editar Registro</h1>
+                </div>
+            </div>
+        </div>
+    </div> <br>
+    <nav class="breadcrumb has-bullet-separator">
+        <div class="container">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/purchase">Pedidos</a></li>
+                <li><a href="#">Agregar</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="notification">
+            <form action="{{ route('purchase_accessory.update', $purchase->id) }}" method="post">
+                @csrf
+                @method('PATCH')
+                {{ csrf_field() }}
+                <div class="field">
+                    <label for="punteo" class="label">Orden</label>
+                    <div class="control">
+                        <input type="text" name="order" value="{{ $purchase->order }}" class="input" required ">
+                </div>
+            </div>
+                <div class=" field">
+                        <label for="punteo" class="label">Precio</label>
+                        <div class="control">
+                            <input type="text" name="price" value="{{ $purchase->price }}" class="input" required ">
+                </div>
+            </div>
+            <div class=" field">
+                            <label for="punteo" class="label">Fecha</label>
+                            <div class="control">
+                                <input type="text" name="date_order" value="{{ $purchase->date_order }}" class="input" required>
+                            </div>
+                        </div>
+                        <select name="provider_id" class="select">
+                            @foreach($provider as $provider)
+                            <option value="{{$provider['id']}}"> {{$provider['name']}} </option>
+                            @endforeach
+                        </select>
+                        <select name="accessory_id" class="select">
+                            @foreach($accessory as $accessory)
+                            <option value="{{$accessory['id']}}"> {{$accessory['name']}} </option>
+                            @endforeach
+                        </select>
+                        <div class=" field">
+                            <div class="control"><br>
+                                <button type="submit" class="button is-info">
+                                    Enviar
+                                </button>
+                            </div>
+                        </div>
+            </form>
+        </div><br><br>
 </body>
 
 </html>
--- resources/views/purchase_accessory/index.blade.php
+++ resources/views/purchase_accessory/index.blade.php
@@ -6,34 +6,53 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
-<body>
+<body><br>    
+    </div>
+    <div class="container">
+        <div class="hero is-link">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-1">Registro de Accesorios</h1>
+                </div>
+            </div>
+        </div>
+    </div> <br>
+    <nav class="breadcrumb breadcrumb has-bullet-separator">
+        <div class="container">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/purchase">Pedidos</a></li>
+            </ul>
+    </nav>
     <div class="container">
         <div class="notification">
-            <table class="table is-fullwidth">
-                <tr>
-                    <th>Pedido</th>
-                    <th>Precio</th>
-                    <th>Fecha de Pedido</th>
-                    <th>Accesorio</th>
-                    <th>Proveedor</th>
-                    <th> </th>
-                    <th> </th>
-                    <th> </th>
-                </tr>
-                @foreach($purchases as $a)
-                <tr>
-                    <td>{{ $a->order }}</td>
-                    <td>{{ $a->price }}</td>
-                    <td>{{ $a->date_order }}</td>
-                    <td>{{ $a->accessory->name }}</td>
-                    <td>{{ $a->provider->name }}</td>
-                    <td><a href="{{ route('purchase_accessory.create') }}">Nuevo</a></td>
-                    <td><a href="{{ route('purchase_accessory.show', $a->id) }}">Detalle</a></td>
-                    <td><a href="{{ route('purchase_accessory.edit', $a->id) }}">Editar</a></td>
-                </tr>
-                @endforeach
+            <div class="level">
+                <div class="level-left">
+                    <a class="button is-info" href="{{ route('purchase_accessory.create') }}">Nuevo</a>
+                </div>
+            </div>
+            <table class=" table is-fullwidth">
+                <thead>
+                    <tr>
+                        <th>Pedido</th>
+                        <th>Precio</th>
+                        <th>Fecha de Pedido</th>
+                        <th>Accesorio</th>
+                        <th>Proveedor</th>
+                    </tr>
+                    @foreach($purchases as $a)
+                    <tr>
+                        <td><a href="{{ route('purchase_accessory.show', $a->id) }}">{{ $a->order }}</td></a>
+                        <td>{{ $a->price }}</td>
+                        <td>{{ $a->date_order }}</td>
+                        <td>{{ $a->accessory->name }}</td>
+                        <td>{{ $a->provider->name }}</td>
+                    </tr>
+                    @endforeach
+                    </tbody>
             </table>
-        </div>
+        </div><br>
     </div>
 </body>
+
 </html>
